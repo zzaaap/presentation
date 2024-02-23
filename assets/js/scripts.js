ee206223a249db54fd5885732e2853e25075955a
zzaaap <iambazzy@gmail.com>
2024-02-23 04:59:54 -0600

    Font hosting

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,11 +25,11 @@ $(function () {
     $.scrollIt({
         upKey: 38,                // key code to navigate to the next section
         downKey: 40,              // key code to navigate to the previous section
-        easing: 'linear',          // the easing function for animation
-        scrollTime: 600,          // how long (in ms) the animation takes
+        easing: 'swing',          // the easing function for animation
+        scrollTime: 1500,          // how long (in ms) the animation takes
         activeClass: 'active',    // class given to the active nav element
         onPageChange: null,       // function(pageIndex) that is called when page is changed
-        topOffset: -75            // offste (in px) for fixed top navigation
+        topOffset: 0            // offste (in px) for fixed top navigation
     });
 
 

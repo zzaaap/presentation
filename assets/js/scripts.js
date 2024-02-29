0b2f17f57548c5ddeda1a466204970e9adab5eb8
zzaaap <iambazzy@gmail.com>
2024-02-29 03:07:56 -0600

    Theme Begin

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,8 @@
 $(document).ready(function () {
   "use strict";
 
+/* ------------------------- Begin Scrolling Progress Button ------------------------- */
+
   // Set up progress bar animation
   var progressPath = document.querySelector(".progress-wrap path");
   var pathLength = progressPath.getTotalLength();
@@ -42,129 +44,9 @@ $(document).ready(function () {
   });
 });
 
-/* ============================================================================= 
------------------------------   Begin Parallax GSAP   -------------------------------
-============================================================================= */
+/* =====================================================================
+------------------------- Begin New Javascript -------------------------
+===================================================================== */
 
-// Animation sequence for Cluster A
-gsap.set(".circle", { yPercent: -5 });
-gsap.set(".dotsBlue", { yPercent: 10 });
-gsap.set(".owlHorned", { yPercent: -20 });
-gsap.set(".clusterA", { yPercent: 5 });
+/* ------------------------- Begin Resizing Box Animation ------------------------- */
 
-gsap.to(".circle", {
-  yPercent: 5,
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".clusterA",
-    scrub: 1
-  },
-});
-
-gsap.to(".dotsBlue", {
-  yPercent: -10,
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".clusterA",
-    scrub: 1
-  },
-});
-
-gsap.to(".owlHorned", {
-  yPercent: 20,
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".clusterA",
-    scrub: 1
-  },
-});
-
-gsap.to(".caption", {
-  yPercent: 100,
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".clusterA",
-    // markers:true,
-    end: "bottom center",
-    scrub: 1
-  },
-});
-
-gsap.to(".clusterA", {
-  yPercent: -5,
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".clusterA",
-    end: "bottom center",
-    scrub: 1
-  },
-});
-
-
-// Animation sequence for Cluster B
-gsap.set(".triangle", { yPercent: 25, rotation: -180 });
-gsap.set(".dotsWhite", { yPercent: 10 });
-gsap.set(".imageB", { yPercent: -20 });
-gsap.set(".clusterB", { yPercent: 5 });
-
-gsap.to(".triangle", {
-  yPercent: -5,
-  rotation: 40,
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".clusterB",
-    scrub: 1
-  },
-});
-
-gsap.to(".dotsWhite", {
-  yPercent: -10,
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".clusterB",
-    scrub: 1
-  },
-});
-
-gsap.to(".imageB", {
-  yPercent: 20,
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".clusterB",
-    scrub: 1
-  },
-});
-
-/* ============================================================================= 
------------------------------   End New GSAP   ----------------------------------
-============================================================================= */
-
-/* ============================================================================= 
------------------------------  Start GSAP Hero Title  --------------------------
-============================================================================= */
-let tl = new TimelineMax();
-
-tl
-	.from( $('.logo__u'), 2, {
-		y: -30
-	} )
-	.from( $('.logo__r'), 2,{
-		rotation: 18,
-		transformOrigin: "100% 50%"
-	},"-=1.5")
-	.from( $('.logo__i'), 2, {
-		y: "100%"
-	},"-=1.5")
-	.from( $('.logo__t-top'), 2, {
-		x: "100%"
-	},"-=1.5")
-	.from( $('.logo__t-bottom'), 2, {
-		y: "-100%"
-	},"-=1.5")
-	.from( $('.logo__y'), 2, {
-		y: "100%"
-	},"-=2");
-
-  /* ============================================================================= 
------------------------------  End GSAP Hero Title  --------------------------
-============================================================================= */

3e74d772e0c83d61bb5613a4fb9a3899f8736657
zzaaap <iambazzy@gmail.com>
2024-03-11 10:24:33 -0500

    Upload final presentation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -47,3 +47,170 @@ $(document).ready(function () {
 /* =====================================================================
 ------------------------- New Javascript -------------------------
 ===================================================================== */
+
+// Detect request animation frame
+var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+
+function loop() {
+
+    Array.prototype.forEach.call(elementsToShow, function(element){
+      if (isElementInViewport(element)) {
+        element.classList.add('is-visible');
+      } else {
+        element.classList.remove('is-visible');
+      }
+    });
+
+    scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+
+// Icon Explode Animation
+
+const iphone = document.querySelector(".iphone");
+const widgets = document.querySelectorAll(".widgets");
+
+gsap.set(iphone, { x: -1600, rotation: 0 });
+gsap.set(widgets, { opacity: 0, scale: 0 });
+
+function iPhoneAnimation() {
+  const tl = gsap.timeline({ defaults: { duration: 1 } });
+  tl.to(iphone, { x: 0 })
+    .to(iphone, { rotation: 0, scale: 0.9 })
+    .to(iphone, { duration: 3, scale: 1 });
+  return tl;
+}
+
+function widgetAnimation() {
+  const tl = gsap.timeline();
+  tl.to(widgets, { duration: 0, opacity: 1 });
+  return tl;
+}
+
+const animations = [
+  {
+    selector: "#app-store",
+    duration: 3,
+    scale: 0.5,
+    x: 1100,
+    y: -100,
+    ease: Power4.easeOut
+  },
+  {
+    selector: "#screen-time",
+    duration: 3,
+    scale: 0.285,
+    x: -775,
+    y: -300,
+    ease: Power2.easeOut
+  },
+  {
+    selector: "#weather",
+    duration: 3,
+    scale: 0.3,
+    x: -1050,
+    y: 350,
+    ease: Power4.easeOut
+  },
+  {
+    selector: "#stocks",
+    duration: 3,
+    scale: 0.55,
+    x: 675,
+    y: -340,
+    ease: Power1.easeOut
+  },
+  {
+    selector: "#folder",
+    duration: 3,
+    scale: 0.6,
+    x: 600,
+    y: -375,
+    ease: Power2.easeOut
+  },
+  {
+    selector: "#fitness",
+    duration: 3,
+    scale: 0.3,
+    x: -390,
+    y: -250,
+    ease: Power1.easeOut
+  },
+  {
+    selector: "#find-my",
+    duration: 3,
+    scale: 0.6,
+    x: 625,
+    y: -400,
+    ease: Power3.easeOut
+  },
+  {
+    selector: "#music",
+    duration: 3,
+    scale: .35,
+    x: -600,
+    y: 300,
+    ease: Power4.easeOut
+  },
+  {
+    selector: "#wallet",
+    duration: 3,
+    scale: .35,
+    x: -650,
+    y: 350,
+    ease: Power4.easeOut
+  },
+  {
+    selector: "#sleep",
+    duration: 3,
+    scale: 0.45,
+    x: 675,
+    y: 275,
+    ease: Power2.easeOut
+  }
+];
+const startTime = 2;
+const masterTimeline = gsap.timeline();
+masterTimeline.add(iPhoneAnimation()).add(widgetAnimation(), startTime);
+
+animations.forEach((animation, index) => {
+  const { selector, duration, scale, x, y, ease } = animation;
+  const element = document.querySelector(selector);
+  masterTimeline.add(
+    gsap.to(element, { duration, scale, x, y, ease }),
+    startTime + (index % 3) / 2
+  );
+});
+
+ScrollTrigger.create({
+  animation: masterTimeline,
+  trigger: ".animation",
+  scrub: 1,
+  pin: true
+  // markers: true
+});
